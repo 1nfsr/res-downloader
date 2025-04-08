d38aef9c0ff8c3996764c22753dcbb69056b6398
1nfsr <10508406+1nfsr@users.noreply.github.com>
2025-04-09 00:02:18 +0800

    update

--- core/app.go
+++ core/app.go
@@ -48,7 +48,7 @@ func GetApp(assets embed.FS, wjs string) *App {
 
 		appOnce = &App{
 			assets:      assets,
-			AppName:     "res-downloader",
+			AppName:     "res-downloader-修改版",
 			Version:     version,
 			Description: "res-downloader是一款集网络资源嗅探 + 高速下载功能于一体的软件，高颜值、高性能和多样化，提供个人用户下载自己上传到各大平台的网络资源功能！",
 			Copyright:   "Copyright © 2023~" + strconv.Itoa(time.Now().Year()),
